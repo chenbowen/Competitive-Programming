a98152a662b9502ce50013144736a2a5935df6bc
chenbowen <cikefengbao@163.com>
2016-10-26 00:41:17 -0400

    fix 12325

--- apoc/chapter7/examples/uva12325.cpp
+++ apoc/chapter7/examples/uva12325.cpp
@@ -63,12 +63,15 @@ int main() {
 		if(s1 > s2) swap(s1, s2), swap(v1, v2);
 		LL ans = 0;
 		if (n / s2 > 60000) {
-			F(i, s1) ans = max(ans, v2*i+(n-s2*i)/s1*v1);
-			F(i, s2) ans = max(ans, v1*i+(n-s1*i)/s2*v2);
+			for(LL i = 0; i <= s1; i++) 
+				ans = max(ans, v2*i+(n-s2*i)/s1*v1);
+			for(LL i = 0; i <= s2; i++) 
+				ans = max(ans, v1*i+(n-s1*i)/s2*v2);
 		}
 		else {
 			int maxs2 = n/s2;
-			F(i, maxs2) ans = max(ans, v2*i+(n-s2*i)/s1*v1);
+			for(LL i = 0; i <= maxs2; i++) 
+				ans = max(ans, v2*i+(n-s2*i)/s1*v1);
 		}
 		printf("Case #%d: %lld\n", ++kase, ans);
 	}
